8487fe70dbd2e99bcacb47a50f01b1eab9912843
Peater68 <prenzo@gmail.com>
2019-05-03 01:42:47 -0700

    Random number fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.spaceship;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -28,6 +29,8 @@ public class TorpedoStore {
     }
   }
 
+  private Random rand = new Random();
+
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
@@ -36,8 +39,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
-    double r = generator.nextDouble();
+    double r = rand.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
